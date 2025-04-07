e482f9f563db031c541af48a025f72c92ef04473
Garf-vader <pricegareth2@gmail.com>
2025-04-07 18:08:05 +0100

    Removed redundant code

--- backend/coffee_api/app/__init__.py
+++ backend/coffee_api/app/__init__.py
@@ -1,11 +1,2 @@
 # app/__init__.py
 from fastapi import FastAPI
-from app import crud
-
-def create_app() -> FastAPI:
-    app = FastAPI()
-
-    # Include the router from crud.py
-    app.include_router(crud.router)
-
-    return app
