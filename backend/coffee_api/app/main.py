d7ea44685413d5e050da810f1940da124b2f1161
Garf-vader <pricegareth2@gmail.com>
2025-04-07 16:35:59 +0100

    Changed port to 9600

--- backend/coffee_api/app/main.py
+++ backend/coffee_api/app/main.py
@@ -3,4 +3,4 @@ import uvicorn
 from app.core import app  # Import the FastAPI app instance
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=9000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=9600, reload=True)
