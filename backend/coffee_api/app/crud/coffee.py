69d21dad92c7c3418a1c11437a1188e872a6b33f
Garf-vader <pricegareth2@gmail.com>
2025-04-15 00:02:06 +0100

    replace review crud with CRUDBase

--- backend/coffee_api/app/crud/coffee.py
+++ backend/coffee_api/app/crud/coffee.py
@@ -1,26 +1,11 @@
 from typing import Optional
 
 from app.models.coffee import CoffeeReview
-from app.schemas.coffee import CoffeeReviewCreate
+from app.schemas.coffee import CoffeeReviewCreate, CoffeeReviewUpdate
 from sqlalchemy.orm import Session
+from app.crud.base import CRUDBase
 
+# Uncomment class to override CRUD or add model-specific functions
+#class CoffeeCRUD(CRUDBase[CoffeeReview, CoffeeReviewCreate, CoffeeReviewUpdate]):
 
-def get_review(db: Session, review_id: int):
-    return db.query(CoffeeReview).filter(CoffeeReview.id == review_id).first()
-
-
-def create_review(db: Session, review: CoffeeReviewCreate) -> CoffeeReview:
-    # Creates a new coffee review in the database
-    db_review = CoffeeReview(
-        drinkType=review.drinkType,
-        roastDarkness=review.roastDarkness,
-        tasteRating=review.tasteRating,
-        price=review.price,
-        notes=review.notes,
-        cafeId=review.cafeId,  # Currently an Integer Reference to Cafe DB, might instead assign each cafe a unique code
-        userId=review.userId # Currently an Integer Reference to Cafe DB, might instead assign each user a unique code
-    )
-    db.add(db_review)
-    db.commit()
-    db.refresh(db_review)
-    return db_review
+coffee_crud = CRUDBase(model=CoffeeReview)
