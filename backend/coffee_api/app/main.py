201c1f8f923a17dee062740e0741fb5fe4c2bccd
garf-vader <pricegareth2@gmail.com>
2025-06-25 21:56:52 +0100

    fix: change server port from 9600 to 80 for proper deployment

--- backend/coffee_api/app/main.py
+++ backend/coffee_api/app/main.py
@@ -2,4 +2,4 @@ import uvicorn
 from app.core import app  # Import the FastAPI app instance
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=9600, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=80, reload=True)
